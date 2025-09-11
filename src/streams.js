cfcb769a1d49aadd7832ac6fc88c80c77f53da7d
Perki <perki@pryv.com>
2025-09-11 14:41:57 +0200

    Added wetness wiping and removing unecessary parentId in def files

--- src/streams.js
+++ src/streams.js
@@ -37,10 +37,15 @@ for (const file of fs.readdirSync(streamsFilePath)) {
 }
 
 function indexStreams (stream, parentId) {
+  if (stream.id == null) throw new Error('missing id for stream: ' + JSON.stringify(stream));
+  if (stream.name == null) throw new Error('missing name for stream: ' + JSON.stringify(stream));
   if (streamsById[stream.id] !== undefined) {
     throw new Error(`Stream with id ${stream.id} already exists, cannot add: ${JSON.stringify(stream)}`);
   }
-  stream.parentId = parentId || null;
+  if (stream.parentId && parentId !== null) {
+    console.log(`${stream.id} does not need parentId : ${stream.parentId}`);
+  }
+  stream.parentId = parentId;
   streamsById[stream.id] = stream;
   if (stream.children) {
     for (const child of stream.children) {
