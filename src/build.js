5ca8faee0f17b043bb7e926ee7419209d04c0515
Perki <perki@pryv.com>
2026-03-02 09:15:08 +0100

    changing build dir

--- src/build.js
+++ src/build.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const basePath = path.resolve(__dirname, '../docs');
+const basePath = path.resolve(__dirname, '../dist');
 
 const { itemsById } = require('./items');
 
